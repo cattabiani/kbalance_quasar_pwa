3e5dffc4ff86bb5f34d9962e07d7dd6d0c89960e
Katta <alessandro.cattabiani@gmail.com>
2025-03-31 22:35:53 +0200

    Updates

---
+++ assets/use-quasar.d666003e.js
@@ -0,0 +1,1 @@
+import{i as a,aq as r}from"./index.a37d1ab0.js";function u(){return a(r)}export{u};
--- assets/use-quasar.f2977d2c.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aq as r}from"./index.d112f7b7.js";function u(){return a(r)}export{u};

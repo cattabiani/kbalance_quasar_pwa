c34f7416d8e3c6e215ee2afbbc45eafecdf4c4e5
Katta <alessandro.cattabiani@gmail.com>
2025-03-31 23:05:23 +0200

    Updates

---
+++ assets/QCardActions.a62da2f0.js
@@ -0,0 +1,1 @@
+import{c as t,ax as r,ay as c,a as n,h as l,b as i}from"./index.293627c5.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=n(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:o.value},i(s.default))}});export{d as Q};
---
+++ assets/use-quasar.715eead7.js
@@ -0,0 +1,1 @@
+import{i as a,aq as r}from"./index.293627c5.js";function u(){return a(r)}export{u};

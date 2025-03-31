147047f7cde72d20c64affea041e2114e3d7363c
Katta <alessandro.cattabiani@gmail.com>
2025-03-31 23:08:02 +0200

    Updates

--- assets/use-quasar.715eead7.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aq as r}from"./index.293627c5.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar.e78684d1.js
@@ -0,0 +1,1 @@
+import{i as a,aq as r}from"./index.d805685e.js";function u(){return a(r)}export{u};

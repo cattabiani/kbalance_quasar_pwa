54be8bb1612acb8af7456a13142faf4ab9e96d1f
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-10-28 10:02:40 +0100

    update packages

--- quasar.config.js
+++ quasar.config.js
@@ -1,237 +1,99 @@
-/* eslint-env node */
+// quasar.config.js
+import { configure } from 'quasar/wrappers';
+import checker from 'vite-plugin-checker'; // <--- add this line
 
-/*
- * This file runs in a Node context (it's NOT transpiled by Babel), so use only
- * the ES6 features that are supported by your Node version. https://node.green/
- */
+export default configure((/* ctx */) => ({
+  boot: [],
 
-// Configuration for your app
-// https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
+  css: ['app.scss'],
 
-const { configure } = require('quasar/wrappers');
+  extras: ['roboto-font', 'material-icons'],
 
-module.exports = configure(function (/* ctx */) {
-  return {
-    // https://v2.quasar.dev/quasar-cli-vite/prefetch-feature
-    // preFetch: true,
+  build: {
+    target: {
+      browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
+      node: 'node20',
+    },
 
-    // app boot file (/src/boot)
-    // --> boot files are part of "main.js"
-    // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: [],
+    vueRouterMode: 'hash',
 
-    // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
-    css: ['app.scss'],
+    publicPath:
+      process.env.NODE_ENV === 'production' ? '/kbalance_quasar_pwa/' : '/',
 
-    // https://github.com/quasarframework/quasar/tree/dev/extras
-    extras: [
-      // 'ionicons-v4',
-      // 'mdi-v7',
-      // 'fontawesome-v6',
-      // 'eva-icons',
-      // 'themify',
-      // 'line-awesome',
-      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+    // remove vite-plugin-checker
+    vitePlugins: [],
+  },
 
-      'roboto-font', // optional, you are not bound to it
-      'material-icons', // optional, you are not bound to it
-    ],
+  devServer: {
+    open: true,
+  },
 
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
-    build: {
-      target: {
-        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
-        node: 'node20',
-      },
+  framework: {
+    config: {},
+    plugins: ['Notify'],
+  },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
-      // vueRouterBase,
-      // vueDevtools,
-      // vueOptionsAPI: false,
+  animations: [],
 
-      // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
+  ssr: {
+    pwa: true,
+    prodPort: 3000,
+    middlewares: ['render'],
+  },
 
-      publicPath:
-        process.env.NODE_ENV === 'production' ? '/kbalance_quasar_pwa/' : '/',
-      // analyze: true,
-      // env: {},
-      // rawDefine: {}
-      // ignorePublicFolder: true,
-      // minify: false,
-      // polyfillModulePreload: true,
-      // distDir
-
-      // extendViteConf (viteConf) {},
-      // viteVuePluginOptions: {},
-
-      vitePlugins: [
-        [
-          'vite-plugin-checker',
+  pwa: {
+    workboxMode: 'generateSW',
+    injectPwaMetaTags: true,
+    swFilename: 'sw.js',
+    manifestFilename: 'manifest.json',
+    useCredentialsForManifestTag: false,
+    extendManifestJson(json) {
+      Object.assign(json, {
+        name: 'kBalance',
+        short_name: 'kBalance',
+        description: 'A simple balance-tracking app to manage finances.',
+        display: 'standalone',
+        start_url: './',
+        theme_color: '#1976D2',
+        background_color: '#ffffff',
+        icons: [
           {
-            eslint: {
-              lintCommand: 'eslint "./**/*.{js,mjs,cjs,vue}"',
-            },
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
-          { server: false },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
         ],
-      ],
+        screenshots: [
+          {
+            src: 'screenshots/transaction.jpeg',
+            sizes: '985x2048',
+            type: 'image/jpeg',
+            form_factor: 'wide',
+          },
+          {
+            src: 'screenshots/conversion.jpeg',
+            sizes: '985x2048',
+            type: 'image/jpeg',
+          },
+        ],
+      });
     },
+  },
 
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
-    devServer: {
-      // https: true
-      open: true, // opens browser window automatically
-    },
+  cordova: {},
+  capacitor: { hideSplashscreen: true },
 
-    // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
-    framework: {
-      config: {},
+  electron: {
+    inspectPort: 5858,
+    bundler: 'packager',
+    packager: {},
+    builder: { appId: 'kbalance-quasar-pwa' },
+  },
 
-      // iconSet: 'material-icons', // Quasar icon set
-      // lang: 'en-US', // Quasar language pack
-
-      // For special cases outside of where the auto-import strategy can have an impact
-      // (like functional components as one of the examples),
-      // you can manually specify Quasar components/directives to be available everywhere:
-      //
-      // components: [],
-      // directives: [],
-
-      // Quasar plugins
-      plugins: ['Notify'],
-    },
-
-    // animations: 'all', // --- includes all animations
-    // https://v2.quasar.dev/options/animations
-    animations: [],
-
-    // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#property-sourcefiles
-    // sourceFiles: {
-    //   rootComponent: 'src/App.vue',
-    //   router: 'src/router/index',
-    //   store: 'src/store/index',
-    //   registerServiceWorker: 'src-pwa/register-service-worker',
-    //   serviceWorker: 'src-pwa/custom-service-worker',
-    //   pwaManifestFile: 'src-pwa/manifest.json',
-    //   electronMain: 'src-electron/electron-main',
-    //   electronPreload: 'src-electron/electron-preload'
-    // },
-
-    // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
-    ssr: {
-      // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-      // will mess up SSR
-
-      // extendSSRWebserverConf (esbuildConf) {},
-      // extendPackageJson (json) {},
-
-      pwa: true,
-
-      // manualStoreHydration: true,
-      // manualPostHydrationTrigger: true,
-
-      prodPort: 3000, // The default port that the production server should use
-      // (gets superseded if process.env.PORT is specified at runtime)
-
-      middlewares: [
-        'render', // keep this as last one
-      ],
-    },
-
-    // https://v2.quasar.dev/quasar-cli-vite/developing-pwa/configuring-pwa
-    pwa: {
-      workboxMode: 'generateSW', // or 'injectManifest'
-      injectPwaMetaTags: true,
-      swFilename: 'sw.js',
-      manifestFilename: 'manifest.json',
-      useCredentialsForManifestTag: false,
-      // useFilenameHashes: true,
-      // extendGenerateSWOptions (cfg) {}
-      // extendInjectManifestOptions (cfg) {},
-      extendManifestJson(json) {
-        Object.assign(json, {
-          name: 'kBalance',
-          short_name: 'kBalance',
-          description: 'A simple balance-tracking app to manage finances.',
-          display: 'standalone',
-          start_url: './',
-          theme_color: '#1976D2', // Quasar primary color
-          background_color: '#ffffff', // Default background color
-          icons: [
-            {
-              src: 'icons/icon-192x192.png', // Make sure these paths are correct
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'icons/icon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-          ],
-          screenshots: [
-            {
-              src: 'screenshots/transaction.jpeg',
-              sizes: '985x2048',
-              type: 'image/jpeg',
-              form_factor: 'wide',
-            },
-            {
-              src: 'screenshots/conversion.jpeg',
-              sizes: '985x2048',
-              type: 'image/jpeg',
-            },
-            // Add more screenshots if necessary
-          ],
-        });
-      },
-      // extendPWACustomSWConf (esbuildConf) {}
-    },
-
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-cordova-apps/configuring-cordova
-    cordova: {
-      // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
-    },
-
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-capacitor-apps/configuring-capacitor
-    capacitor: {
-      hideSplashscreen: true,
-    },
-
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/configuring-electron
-    electron: {
-      // extendElectronMainConf (esbuildConf)
-      // extendElectronPreloadConf (esbuildConf)
-
-      // specify the debugging port to use for the Electron app when running in development mode
-      inspectPort: 5858,
-
-      bundler: 'packager', // 'packager' or 'builder'
-
-      packager: {
-        // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-        // OS X / Mac App Store
-        // appBundleId: '',
-        // appCategoryType: '',
-        // osxSign: '',
-        // protocol: 'myapp://path',
-        // Windows only
-        // win32metadata: { ... }
-      },
-
-      builder: {
-        // https://www.electron.build/configuration/configuration
-
-        appId: 'kbalance-quasar-pwa',
-      },
-    },
-
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-browser-extensions/configuring-bex
-    bex: {
-      contentScripts: ['my-content-script'],
-
-      // extendBexScriptsConf (esbuildConf) {}
-      // extendBexManifestJson (json) {}
-    },
-  };
-});
+  bex: { contentScripts: ['my-content-script'] },
+}));

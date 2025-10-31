5e14c14cebeb1f97bea0a32cc3d5123572fd4a16
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-11-01 00:41:36 +0100

    Updates

--- assets/QCardActions-BfX6BrJ-.js
+++
@@ -1,1 +0,0 @@
-import{c as e,aP as c,aQ as r,a as n,h as l,b as i}from"./index-CvXSrUDU.js";const d=e({name:"QCardActions",props:{...c,vertical:Boolean},setup(a,{slots:s}){const o=r(a),t=n(()=>`q-card__actions ${o.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:t.value},i(s.default))}});export{d as Q};
